29d3715d4c40b8d9676eca7d52ca20d074dd9fbf
MalekMneri <47902348+MalekMneri@users.noreply.github.com>
2020-09-06 18:38:53 +0100

    first commi!

---
+++ myRecipeApp/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe',
+      'This is a new recipe for food',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700%2C636')
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
