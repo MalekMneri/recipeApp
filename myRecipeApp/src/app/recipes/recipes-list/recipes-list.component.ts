66a5d7a074b49c6e9b64128fcb60cba2d2623a1a
MalekMneri <47902348+MalekMneri@users.noreply.github.com>
2020-09-06 19:58:53 +0100

    changed shopping-list/items/recipes html/css

--- myRecipeApp/src/app/recipes/recipes-list/recipes-list.component.ts
+++ myRecipeApp/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -10,7 +10,12 @@ export class RecipesListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A test recipe',
       'This is a new recipe for food',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700%2C636')
+      'https://images.immediate.co.uk/production/volatile/sites' +
+      '/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700%2C636'),
+    new Recipe('Second test recipe',
+      'This is a newer recipe for food',
+      'https://images.immediate.co.uk/production/volatile/sites' +
+      '/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700%2C636')
   ];
   constructor() { }
 
